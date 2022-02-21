dbb38724e91a4a97fdc6e2b6a139fab5528f4708
pauloborges95 <pauloborgesmartim@gmail.com>
2022-02-20 22:11:42 -0300

    projeto similar a sistema de banco

---
+++ Banco/Conta.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Banco
+{
+    class Conta
+    {
+        //propriedades
+        public string Titular { get; set; } // nome pode ser alterado
+        public int Numero { get; private set; } //número da conta não pode ser alterado
+        public double Saldo { get; private set; }//é possível ser alterado pela ação do usuário e não do progamador
+
+        //contrutores
+        public Conta(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+                
+        }
+
+        public Conta(int numero, string titular, double saldo) : this(numero, titular) // palavra this para reaproveitar os atirbutos
+        {
+            Saldo = saldo;
+        }
+        //métodos
+        public void Deposito(double quantia) //utiliza o void porque não retorna valor
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+
+        //é o que vai mostrar as informações na tela
+        public override string ToString()
+        {
+            return "Conta: "
+                + Numero
+                + ", titular: "
+                + Titular
+                + ", saldo:  $"
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PROCESSO INTERAÇÃO COM O USUARIO
+
+            Conta conta; // classe para chamar o construtor
+            Console.WriteLine("Digite o número da sua conta");
+            int numero = int.Parse(Console.ReadLine()); //variável temporaria
+            Console.WriteLine("Digite o nome do titular");
+            string titular = Console.ReadLine();// variavel auxiliar
+            Console.WriteLine("Haverá depósito inicial?(s/n)");
+            char resp = char.Parse(Console.ReadLine());
+
+            if(resp == 's' || resp == 'S')//a tecla || significa OU
+            {
+                Console.WriteLine("Digite o valor do depósito:");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);//variavel auxiliar
+                conta = new Conta(numero, titular, depositoInicial); // atribuir e utilizar o construtor
+                
+            }
+
+            else
+            {
+                conta = new Conta(numero, titular);
+            }
+
+            Console.WriteLine("dados da conta");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.WriteLine("valor do deposito:");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);//chama a operação da classe
+            Console.WriteLine("Dados atualizados");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.WriteLine("Valor do saque:");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("dados atualizados");
+            Console.WriteLine(conta);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
